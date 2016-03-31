9aeb278aaffed2f5906633f6d6f95aad42ac4f68
ekmccaffrey <mccaffrey.erin@yahoo.com>
2016-03-31 13:31:25 -0400

    attached db and logic to connect db (did not remove hardcoded fake data)

--- Eirynne.Posts.Mock/PostSelector.cs
+++ Eirynne.Posts.Mock/PostSelector.cs
@@ -1,9 +1,79 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
 
 namespace Eirynne.Posts.Mock
 {
+
+    public class Database
+    {
+        static void Main()
+        {
+            //string strAccessConn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=..\\..\\Eirynne.BlogWebsite.ACCDB";
+            string strAccessConn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=EirynneBlogWebsite.MDB";
+
+            string strAccessSelect = "SELECT Posts.title, Posts.postDate, Tags.tag FROM Posts, Tags, PostsTags WHERE Posts.ID = PostsTags.postID AND Tags.ID = PostsTags.tagID";
+
+            DataSet myDataSet = new DataSet();
+            OleDbConnection myAccessConn = null;
+            try
+            {
+                myAccessConn = new OleDbConnection(strAccessConn);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: Failed to create a database connection. \n{0}", ex.Message);
+                return;
+            }
+
+            try
+            {
+                OleDbCommand myAccessCommand = new OleDbCommand(strAccessSelect, myAccessConn);
+                OleDbDataAdapter myDataAdapter = new OleDbDataAdapter(myAccessCommand);
+
+                myAccessConn.Open();
+                myDataAdapter.Fill(myDataSet, "Posts");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: Failed to retrieve required data from the DataBase.\n{0}", ex.Message);
+                return;
+            }
+            finally
+            {
+                myAccessConn.Close();
+            }
+
+            DataTableCollection dta = myDataSet.Tables;
+            foreach (DataTable dt in dta)
+            {
+                Console.WriteLine("Found data table {0}", dt.TableName);
+            }
+
+            Console.WriteLine("{0} tables in data set", dta.Count);
+
+            Console.WriteLine("{0} rows in Posts table", myDataSet.Tables["Posts"].Rows.Count);
+
+            Console.WriteLine("{0} columns in Posts table", myDataSet.Tables["Posts"].Columns.Count);
+            DataColumnCollection drc = myDataSet.Tables["Posts"].Columns;
+            int i = 0;
+            foreach (DataColumn dc in drc)
+            {
+                Console.WriteLine("Column name[{0} is {1}, of type {2}", i++, dc.ColumnName, dc.DataType);
+            }
+
+            DataRowCollection dra = myDataSet.Tables["Posts"].Rows;
+            foreach (DataRow dr in dra)
+            {
+                Console.WriteLine("CategoryName[{0}] was posted {1}  and is tagged {2}", dr[0], dr[1], dr[2]);
+            }
+
+            if (System.Diagnostics.Debugger.IsAttached) Console.ReadLine();
+        }
+    }
+
     public class PostSelector : IPostSelector
     {
         private readonly List<Post> _posts;
